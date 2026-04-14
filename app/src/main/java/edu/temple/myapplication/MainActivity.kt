34d29d3215024f9fdf7dabb2effe8a488d8c17f3
Johnny Tan <tannjohnny03@gmail.com>
2026-04-14 12:16:24 -0400

    Completed in class activity

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -2,7 +2,10 @@ package edu.temple.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast // Added for demonstration
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,11 +14,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
-
+            startLogic()
         }
-        
+
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            stopLogic()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_start -> {
+                startLogic()
+                true
+            }
+            R.id.action_stop -> {
+                stopLogic()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun startLogic() {
+        Toast.makeText(this, "Starting...", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun stopLogic() {
+        Toast.makeText(this, "Stopping...", Toast.LENGTH_SHORT).show()
     }
 }
